c837b49fb7293f361ed136ec56ed85e5dfeab919
fosterlynn02 <fosterlynnboulin@gmail.com>
2018-08-10 14:17:19 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,36 @@
 # Code your instances here
+
+class GardenGnome
+ 
+ def initialize (hat_color="red")
+   @name 
+   @age 
+   @personality="evil"
+  @hat_color=hat_color
+  
+  def name 
+    @name
+  end 
+  
+  def age 
+    @age
+  end 
+  
+  def personality="evil"
+      @evil
+  end 
+      
+      def hat_color=hat_color 
+        @hat_color=hat color 
+        
+      end 
+      
+      
+        
+  
+      
+      
+ 
+ 
+  
+  
